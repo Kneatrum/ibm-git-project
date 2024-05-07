b8158f066ab2116c2d211526b7514bee59c7a1c5
Martin Mwiti <martin.mwiti.n@gmail.com>
2024-05-07 13:50:31 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,25 @@
 
+#!/bin/bash
+
+# Function to calculate simple interest
+calculate_simple_interest() {
+    principal=$1
+    rate=$2
+    time=$3
+
+    # Calculate simple interest
+    interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
+    echo "Simple Interest: $interest"
+}
+
+# Read principal amount from user
+read -p "Enter principal amount: " principal
+
+# Read rate of interest from user
+read -p "Enter rate of interest (in percentage): " rate
+
+# Read time period from user
+read -p "Enter time period (in years): " time
+
+# Call the function to calculate simple interest
+calculate_simple_interest $principal $rate $time
